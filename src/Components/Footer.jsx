30b0355f9992d9b29c9a3e7e657936e23d322539
Earthpasserby <iwuno68@gmail.com>
2024-05-02 02:33:35 +0100

    Homepage commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <footer className="footer">
         <Container fluid style={{ backgroundColor: "#161616" }}>
           <Row className="centered-row2 justify-content-center">
-            <Col md={5}>
+            <Col md={5} className="mt-5">
               <Image src="/./The Brand Plug Logo.png" className="img-fluid" />
               <p className="mt-4 p1">
                 At The Brand Plug, we are the architects of possibility, the
@@ -19,30 +19,37 @@ const Footer = () => {
                 creativity, fueled by passion, and driven by a relentless
                 pursuit of excellence.
               </p>
-              <div className="mt-4">
+              <div className="mt-4 pic">
                 <Image src="/./Facebook.png" className="img-fluid" />
                 <Image src="/./x.png" className="img-fluid" />
                 <Image src="/./ig.png" className="img-fluid" />
               </div>
             </Col>
-            <Col md={4}>
-              <h5>Services</h5>
-              <p>Branding</p>
-              <p>Digital Marketing</p>
-              <p>Search Engine Optimization (SEO)</p>
-              <p> Growth Strategies</p>
-              <p> Content Creation</p>
-              <p> Media Planning and Buying</p>
-              <p> Public Relations (PR)</p>
-              <p> Consulting Services</p>
+            <Col md={4} className="">
+              <div className="p2">
+                <h5>Services</h5>
+                <p>Branding</p>
+                <p>Digital Marketing</p>
+                <p>Search Engine Optimization (SEO)</p>
+                <p> Growth Strategies</p>
+                <p> Content Creation</p>
+                <p> Media Planning and Buying</p>
+                <p> Public Relations (PR)</p>
+                <p> Consulting Services</p>
+              </div>
             </Col>
-            <Col md={3}>
-              <h5>OTHERS</h5>
-              <p>Projects</p>
-              <p>Blog</p>
-              <p>Contact Us</p>
+            <Col md={3} className=" ">
+              <div className="p2">
+                <h5>OTHERS</h5>
+                <p>Projects</p>
+                <p>Blog</p>
+                <p>Contact Us</p>
+              </div>
             </Col>
-            <hr />
+
+            <div className="footer-content">
+              <p className="copyright">Copyright © 2024. The Brand Plug</p>
+            </div>
           </Row>
         </Container>
       </footer>
